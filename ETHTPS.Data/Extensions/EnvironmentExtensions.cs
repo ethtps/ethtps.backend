236d3f12d235e5269febbfe8c63c2fa85436fa34
Mister_Eth <89896648+Mister-Eth@users.noreply.github.com>
2023-03-18 07:52:55 +0200

    I don't know what I'm doing

--- ETHTPS.Data/Extensions/EnvironmentExtensions.cs
+++ ETHTPS.Data/Extensions/EnvironmentExtensions.cs
@@ -7,21 +7,28 @@ namespace ETHTPS.Data.Core.Extensions
     {
         public static string GetEnvVarValue(string name)
         {
-            var env = Environment.GetEnvironmentVariable(ENV, EnvironmentVariableTarget.Machine);
+            var env = GetEnvironmentVariableFromAnywherePriorityWiseOrThrow(ENV);
             if (string.IsNullOrWhiteSpace(env))
                 throw new ArgumentNullException(ENV);
             string? result = null;
-            switch (env)
+            result = env switch
             {
-                case "DEVELOPMENT":
-                    result = Environment.GetEnvironmentVariable($"{name}_{env}", EnvironmentVariableTarget.Machine);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(ENV);
-            }
+                "DEVELOPMENT" => GetEnvironmentVariableFromAnywherePriorityWiseOrThrow($"{name}_{env}"),
+                _ => throw new ArgumentOutOfRangeException(ENV)
+            };
             if (string.IsNullOrEmpty(result))
                 throw new Exception($"{name} not defined");
             else return result;
         }
+
+        /// <summary>
+        /// Just get it from anywhere, I don't really care
+        /// </summary>
+        private static string GetEnvironmentVariableFromAnywherePriorityWiseOrThrow(string name) => 
+            Environment.GetEnvironmentVariable(name) ?? 
+            Environment.GetEnvironmentVariable(name, 
+                EnvironmentVariableTarget.Process) ?? 
+            Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.User) ?? 
+            Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Machine) ?? throw new ArgumentNullException(name);
     }
 }
