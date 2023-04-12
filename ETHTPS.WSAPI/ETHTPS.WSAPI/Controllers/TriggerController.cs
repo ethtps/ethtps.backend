ceb8bea2c2691afe17946196c936e724b36b2199
Mister_Eth <89896648+Mister-Eth@users.noreply.github.com>
2023-04-12 22:05:34 +0300

    Remove bloat

--- ETHTPS.WSAPI/ETHTPS.WSAPI/Controllers/TriggerController.cs
+++ ETHTPS.WSAPI/ETHTPS.WSAPI/Controllers/TriggerController.cs
@@ -1,10 +1,12 @@
 ﻿using ETHTPS.Data.Core.Models.LiveData.Triggers;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ETHTPS.WSAPI.Controllers
 {
     [Route("/api/v3/wsapi/trigger/[action]")]
+    [Authorize(Roles = "Microservice")]
     public class TriggerController : Controller
     {
         [HttpPost]
